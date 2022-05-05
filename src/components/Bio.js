877e22e712ec21a59ad06f22eb590a0b97173e9c
venkat-p-dev <104988076+venkat-p-dev@users.noreply.github.com>
2022-05-05 08:23:49 -0400

    Add files via upload

---
+++ src/components/Bio.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from './Bio.module.css'
+import { getGravatarURL } from '../utils/getGravatarURL'
+
+function Bio(props) {
+  let photoURL = getGravatarURL({
+    email: "test1@example.com",
+    size: 56,
+  })
+
+  return (
+    <div className={`
+      ${styles.Bio}
+      ${props.className || ''}
+    `}>
+      <img src={photoURL} alt="Me" />
+      <p>
+        Create a blog with a single command, by{' '}
+        <a href="">Venkat Pingili</a>.
+        <br />
+        Based on{' '}
+        <a href="https://facebook.github.io/create-react-app/">
+          create-react-app
+        </a>
+        , <a href="https://mdxjs.com/">MDX</a>, and{' '}
+      </p>
+    </div>
+  )
+}
+
+export default Bio
---
+++ src/siteMetadata.js
@@ -0,0 +1,11 @@
+export default {
+  // The blog's title as it appears in the layout header, and in the document
+  // <title> tag.
+  title: "Venkat Pingili Blog",
+
+  author: "venkat Pingili",
+  description: "",
+
+  // The number of posts to a page on the site index.
+  indexPageSize: 5,
+}
