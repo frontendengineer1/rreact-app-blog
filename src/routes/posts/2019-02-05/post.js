8a1418a1eff043843231c8735ad7e7350228063b
venkat-p-dev <104988076+venkat-p-dev@users.noreply.github.com>
2022-05-05 08:31:18 -0400

    Add files via upload

---
+++ src/routes/posts/2019-02-05/post.js
@@ -0,0 +1,9 @@
+export default {
+  title: `Learn How To Build This Blog From Scratch`,
+  tags: ['react', 'navi'],
+  spoiler: "An online course with loads of live demos and exercises.",
+  getContent: () => import('./document.mdx'),
+}
+
+
+
